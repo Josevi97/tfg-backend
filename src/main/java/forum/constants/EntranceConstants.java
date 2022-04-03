cc86eb79f703cfb4666cff0f15d1c1eb0bd8ab5d
Josevi97 <josevi00@hotmail.com>
2022-04-03 19:45:16 +0200

    tfg done?

--- src/main/java/forum/constants/EntranceConstants.java
+++ src/main/java/forum/constants/EntranceConstants.java
@@ -2,7 +2,7 @@ package forum.constants;
 
 public class EntranceConstants {
     public static final int MIN_TITLE_SIZE = 4;
-    public static final int MAX_TITLE_SIZE = 15;
+    public static final int MAX_TITLE_SIZE = 30;
     public static final int MIN_BODY_SIZE = 4;
     public static final int MAX_BODY_SIZE = 3000;
 }
