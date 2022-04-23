ac94e6263b7987e01394d7222cb3d8eb6b7b6ed4
Josevi97 <josevi00@hotmail.com>
2022-04-23 14:13:50 +0200

    password size limit modified

--- src/main/java/forum/constants/AccountConstants.java
+++ src/main/java/forum/constants/AccountConstants.java
@@ -8,6 +8,6 @@ public class AccountConstants {
     public static final int MIN_USERNAME_SIZE = 4;
     public static final int MAX_USERNAME_SIZE = 15;
     public static final int MIN_PASSWORD_SIZE = 4;
-    public static final int MAX_PASSWORD_SIZE = 20;
+    public static final int MAX_PASSWORD_SIZE = 255;
     public static final int MAX_DESCRIPTION_SIZE = 200;
 }
